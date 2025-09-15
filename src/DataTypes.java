8ce7925e87ad95f5203f7edb8c2e5cdb9772fc6f
Stefan Lu <stefan.lu@mail.utoronto.ca>
2025-09-15 09:50:57 -0400

    test fix qfwasdfasdf

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,7 +3,7 @@ public class DataTypes {
     public static long sum(int[] numbers) {
         int s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
+        for (int x = 1; x <= numbers.length; x++) {
             s += x;
         }
         return s;
